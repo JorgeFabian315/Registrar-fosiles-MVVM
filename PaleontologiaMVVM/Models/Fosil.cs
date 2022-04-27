0aeca64dab9cadedc533184a8277e55164947e30
Jorge Fabian Liñan Alcala <201G0264@rcarbonifera.tecnm.mx>
2022-04-27 16:17:39 -0500

    Agregar archivos de proyecto.

---
+++ PaleontologiaMVVM/Models/Fosil.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PaleontologiaMVVM.Models
+{
+    public class Fosil
+    {
+        public string Nombre { get; set; } = "";
+        public string Imagen { get; set; } = "";
+        public string Descripcion { get; set; } = "";
+        public string FechaDescubrimiento { get; set; } = "";
+
+    }
+}
---
+++ PaleontologiaMVVM/Views/PrincipalView.xaml.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace PaleontologiaMVVM.Views
+{
+    /// <summary>
+    /// Lógica de interacción para PrincipalView.xaml
+    /// </summary>
+    public partial class PrincipalView : Window
+    {
+        public PrincipalView()
+        {
+            InitializeComponent();
+        }
+
+        private void btnMinimizar_Click(object sender, RoutedEventArgs e)
+        {
+            WindowState = WindowState.Minimized;
+        }
+
+        private void btnCerar_Click(object sender, RoutedEventArgs e)
+        {
+            this.Close();
+        }
+
+        private void Window_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+            if (e.LeftButton == MouseButtonState.Pressed)
+            {
+                this.DragMove();
+            }
+        }
+
+        private void Window_MouseDoubleClick(object sender, MouseButtonEventArgs e)
+        {
+            //WindowState = WindowState.Maximized;
+
+        }
+    }
+}
